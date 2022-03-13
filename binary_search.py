69851d4ed5790d039781e59c44764353ce325b0e
stella-oh <funnymarketer@gmail.com>
2022-03-14 00:12:23 +0900

    study1

--- binary_search.py
+++ binary_search.py
@@ -5,3 +5,23 @@ Spyder Editor
 This is a temporary script file.
 """
 
+def binary_search(list, item):
+    low =0
+    high = len(list)-1
+    
+    while low <= high:
+        mid = (low + high) //2
+        guess = list[mid]
+        if guess == item:
+            return mid
+        if guess >item:
+            high = mid -1
+        else:
+            low = mid +1
+    return None
+
+my_list = [1,2,3,4,4,5,6,6,7]
+
+print(binary_search(my_list, 3))
+
+print(binary_search(my_list, -100))
